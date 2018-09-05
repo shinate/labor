2bebb844ba52b41e19cf250cfce0e87240b91372
shinate <shine.wangrs@gmail.com>
2018-09-05 23:28:42 +0800

    SAVE

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,5 +2,5 @@
 
 module.exports = function (gulp, PLUGIN, CONF) {
     gulp.task('build', ['css', 'js', 'resources', 'fonts', 'images'/*, 'version', 'bridge'*/]);
-    gulp.task('build-dev', ['css-dev', 'js-dev', 'fonts-dev', 'images-dev']);
+    // gulp.task('build-dev', ['css-dev', 'js-dev', 'fonts-dev', 'images-dev']);
 };
--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,20 +1,11 @@
 'use strict';
 
 module.exports = function (gulp, PLUGIN, CONF) {
-
     gulp.task('fonts', function () {
         return gulp.src([
             CONF.src + '/fonts/*',
         ])
             .pipe(PLUGIN.plumber())
             .pipe(gulp.dest(CONF.release + '/fonts'));
-    });
-
-    gulp.task('fonts-dev', function () {
-        return gulp.src([
-            CONF.src + '/fonts/*',
-        ])
-            .pipe(PLUGIN.plumber())
-            .pipe(gulp.dest(CONF.build + '/fonts'));
     });
 };
