c72f6a1bfaec5b083a36722ec63059c60981fb7b
shinate <shine.wangrs@gmail.com>
2018-11-28 12:53:56 +0800

    release

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function (gulp, PLUGIN, CONF) {
-    gulp.task('default', ['build', 'release']);
+    gulp.task('default', ['build']);
     gulp.task('build', ['script', 'style']);
     gulp.task('watch', ['script:watch', 'style:watch']);
 };
