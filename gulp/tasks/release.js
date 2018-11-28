5739b4392e74ae19a98e3ed71ee8c7811ae269c0
shinate <shine.wangrs@gmail.com>
2018-11-28 12:47:28 +0800

    release

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -4,9 +4,14 @@ var execa = require('execa')
 
 module.exports = function (gulp, PLUGIN, CONF) {
     gulp.task('release', function (cb) {
-        execa.shellSync('git add .');
-        execa.shellSync('git commit -m "release"');
-        execa.shellSync('git push');
+        try {
+            execa.shellSync('git add .');
+            execa.shellSync('git commit -m "release"');
+            execa.shellSync('git push');
+            console.log(new Date().toTimeString());
+        } catch (e) {
+            console.log(e)
+        }
         cb();
     });
 };
